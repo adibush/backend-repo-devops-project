97d20f4760ae341e43f49d5fd0ed6b92f312e386
adi <adibush01@gmail.com>
2026-07-18 21:34:42 +0300

    updated

--- src/database.py
+++ src/database.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 client = MongoClient(
-    "mongodb://admin:password@host.docker.internal:27017/"
+    "mongodb://admin:password@mongodb-service:27017/"
 )
 
 db = client["hotel_db"]
