e7cba92c50ad37d23fcbe55a80e4d01b5c8ca5a8
adi <adibush01@gmail.com>
2026-07-25 17:32:07 +0300

    updated

--- src/database.py
+++ src/database.py
@@ -1,14 +1,21 @@
+import os
+
 from pymongo import MongoClient
 
-client = MongoClient(
-    "mongodb://admin:password@mongodb-service:27017/"
-)
+mongo_host = os.getenv("MONGO_HOST")
+mongo_port = os.getenv("MONGO_PORT")
+database_name = os.getenv("DATABASE_NAME")
+mongo_username = os.getenv("MONGO_USERNAME")
+mongo_password = os.getenv("MONGO_PASSWORD")
 
-db = client["hotel_db"]
+mongo_uri = f"mongodb://{mongo_username}:{mongo_password}@{mongo_host}:{mongo_port}/"
+
+client = MongoClient(mongo_uri)
+
+db = client[database_name]
 
 reservations = db["reservations"]
 
 hotels = db["hotels"]
-
 
 
