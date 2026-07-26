4f8f572bc059bd9399043a20d2c735a9b2ded84b
adi <adibush01@gmail.com>
2026-07-26 17:05:00 +0300

    Support MongoDB replica set connection

--- src/database.py
+++ src/database.py
@@ -1,14 +1,28 @@
 import os
+from urllib.parse import quote_plus
 
 from pymongo import MongoClient
 
 mongo_host = os.getenv("MONGO_HOST")
+mongo_hosts = os.getenv("MONGO_HOSTS")
 mongo_port = os.getenv("MONGO_PORT")
 database_name = os.getenv("DATABASE_NAME")
+mongo_replica_set = os.getenv("MONGO_REPLICA_SET")
+mongo_auth_source = os.getenv("MONGO_AUTH_SOURCE", "admin")
 mongo_username = os.getenv("MONGO_USERNAME")
 mongo_password = os.getenv("MONGO_PASSWORD")
 
-mongo_uri = f"mongodb://{mongo_username}:{mongo_password}@{mongo_host}:{mongo_port}/"
+encoded_username = quote_plus(mongo_username)
+encoded_password = quote_plus(mongo_password)
+
+if mongo_hosts and mongo_replica_set:
+    mongo_uri = (
+        f"mongodb://{encoded_username}:{encoded_password}@"
+        f"{mongo_hosts}/{database_name}"
+        f"?replicaSet={mongo_replica_set}&authSource={mongo_auth_source}"
+    )
+else:
+    mongo_uri = f"mongodb://{encoded_username}:{encoded_password}@{mongo_host}:{mongo_port}/"
 
 client = MongoClient(mongo_uri)
 
